e4f17cb23bb54955be25f68f03625e631958c53c
Mikki.Lee <mikki.clay87@gmail.com>
2025-03-11 14:36:52 -0400

    adding javascript to cart page

--- Frontend/product.js
+++ Frontend/product.js
@@ -1,1 +1,38 @@
-<Script></Script>
+const cartIcon = document.querySelector("#cart-icon");
+const cart = document.querySelector(".cart");
+const cartClose = document.querySelector("#cart-close");
+cartIcon.addEventListener("click", () => cart.classList.add("active"));
+cartClose.addEventListener("click", () => cart.classList.remove("active"));
+
+const addCartButtons = document.querySelector(".add-cart");
+addCartButtons.forEach(button => {
+   button.addEventListener ("click", event => {
+    const productBox = event.target.closest (".product-box");
+    addToCart(productBox);
+   }); 
+});
+
+const cartContent = document.querySelector(".cart-content");
+const addToCart = productBox => {
+    const productImgSrc = productBox.querySelector ("img").src;
+    const productTitle = productBox.querySelector (".product-title").textContent;
+    const productPrice = productBox.querySelector (".price").textContent;
+
+    const cartBox = document.createElement("div");
+    cartBox.classList.add("cart-box");
+    cartBox.innerHTML = `
+     <img src="${productImgSrc}" class="cart-img">
+                    <div class="cart-detail">
+                        <h2 class="cart-product-title">${productTitle}</h2>
+                        <span class="cart-price">${productPrice}</span>
+                        <div class="cart-quantity">
+                            <button id="decrement">-</button>
+                            <span class="number">1</span>
+                            <button id="increment">+</button>
+                        </div>
+                    </div>
+                    <i class="ri-delete-bin-line cart-remove"></i>
+    `;
+
+    cartContent.appendChild(cartBox);
+};
